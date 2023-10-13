ee2da89b30de046951077cbc31bf3d9d4943340b
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-13 20:00:16 +0300

    add upr1 in script1

---
+++ HW2/js/script1.js
@@ -0,0 +1,8 @@
+let name = prompt('ваше имя', "");
+let age = prompt('возраст', "");
+let city = prompt('город', "");
+let phone = prompt('телефон', "");
+let email = prompt('электронная почта', "");
+let company = prompt('место работы', "");
+
+console.log(`Меня зовут $(name). Мне $(age) лет. Я проживаю в городе $(city) и работаю в компании $(company). Мои контактные данные: $(phone), $email  `)
