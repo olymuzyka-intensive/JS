43d96377240c07e8ec9482e04f0a4ebd5665c18d
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-13 23:52:04 +0300

    add 4 varianta upr3

---
+++ HW2/js/script3.js
@@ -0,0 +1,25 @@
+let st = prompt('введите 6 цифр', '');
+
+if (Number(st[0])+Number(st[1])+Number(st[2]) == Number(st[3])+Number(st[4])+Number(st[5])) alert('да'); else alert('нет');
+
+
+
+//еще рабочие варианты решения, потом нашла опечатку выше
+
+// if (- (st[0]) - (st[1]) - (st[2]) == - (st[3]) - (st[4]) - (st[5])) alert('да'); else alert('нет');
+
+
+// var str = prompt(); 
+// if ((+str[0] + +str[1] + +str[2]) == (+str[3] + +str[4] + +str[5])) {
+// alert('da');
+// } else {
+// alert('net');
+// }
+
+
+// var a=prompt();
+// if (Number(a[0])+Number(a[1])+Number(a[2]) == Number(a[3])+Number(a[4])+Number(a[5]))
+// alert ('true');
+// else
+// alert ('false');
+
