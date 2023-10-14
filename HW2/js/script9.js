4bf1d21af1e56cfa3510ca397f1d6dbeae6d7f41
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-14 18:00:32 +0300

    fix compulation error

--- HW2/js/script9.js
+++ HW2/js/script9.js
@@ -1,23 +1,18 @@
 let dni=prompt('введите количество дней', '');
 let year = dni / 365;
-let month = year / 12;
-let week = month / 4.33;
-let hour = week / 168;
-let minute = hour / 60;
-let sec = minute / 60;
-
-document.write('в введенном количестве дней ' + dni + ' '); 
-if ( dni < 325) (console.log('Меньше года')) ||  (document.write((year + ' лет ')) &&  document.write((month + ' месяцев ')) && document.write((week + ' недель ')));
-if ( dni < 31) (console.log('Меньше месяца')) ||  (document.write((month + ' месяцев ')) && document.write((week + ' недель ')))
-if ( dni < 7) (console.log('Меньше недели')) ||  (document.write((week + ' недель '))); 
-document.write(hour + ' часов '); document.write(minute + ' минут '); document.write(sec + ' секунд ' );
+let month = dni / 31;
+let week = dni / 7;
+let hour = dni * 24;
+let minute = hour * 60;
+let sec = minute * 60;
 
 
 
-// document.write('в введенном количестве дней ' + dni + ' '); 
-// if ( dni < 325) console.log('Меньше года') ||  document.write((year + ' лет ')); 
-// if ( dni < 31) console.log('Меньше месяца') ||  document.write((month + ' месяцев ') ); 
-// if ( dni < 7) console.log('Меньше недели') ||  document.write((week + ' недель ') ); 
-// document.write(hour + ' часов '); 
-// document.write(minute + ' минут '); 
-// document.write(sec + ' секунд ' );
+
+console.log('в введенном количестве дней ' + dni + ''); 
+if ( dni < 365) {console.log('Меньше года')} else {console.log(year + ' лет ')}; 
+if ( dni < 31) {console.log('Меньше месяца')} else {console.log(month + ' месяцев ')}; 
+if ( dni < 7) {console.log('Меньше недели')} else {console.log(week + ' недель ')}; 
+console.log(hour + ' часов '); 
+console.log(minute + ' минут '); 
+console.log(sec + ' секунд ' );
