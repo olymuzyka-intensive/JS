2d5109f45c46efe3d0048b84c5f92cecbaf317cd
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-14 09:41:27 +0300

    add upr7

--- HW2/js/script5.js
+++ HW2/js/script5.js
@@ -1,9 +1,9 @@
-let a = 10;
-let b = 2;
-let summ = a + b;
-console.log('', summ);
-console.log('', a - b);
-console.log('', a * b);
-console.log('', a / b);
+// let a = 10;
+// let b = 2;
+// let summ = a + b;
+// console.log('', summ);
+// console.log('', a - b);
+// console.log('', a * b);
+// console.log('', a / b);
 
-if (summ > 1) console.log('квадрат суммы', summ ** 2);
+// if (summ > 1) console.log('квадрат суммы', summ ** 2);
