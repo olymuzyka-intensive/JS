88c9ceaac5eaf441acb2055b1ab78a422dd80ef9
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-17 22:14:20 +0300

    add upr4

---
+++ HW3/js/script4.js
@@ -0,0 +1,7 @@
+// ограничение от 1 до 5 необязательно
+let a = prompt('vvedite chislo to 1 do 5');
+let summ = 0;
+for (let i = 0; i <= a; i++) {
+    summ += i;
+}
+console.log('сумма чисел в числе', summ)
