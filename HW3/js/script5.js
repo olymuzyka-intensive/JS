15e8f9aa8c94540ad69b99b5f870c6fb976894b2
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-17 22:54:29 +0300

    add upr5 for, while poisk reshenia

---
+++ HW3/js/script5.js
@@ -0,0 +1,12 @@
+let i = 8;
+for (i; i <=56; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+
+// while (i <= 56) {
+//     if (i % 2 == 0) {
+//         console.log(i++);
+//     }
+// }
