75ecc09eb48e5f2caacb3df84e97216a9dd932fc
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-17 23:09:23 +0300

    fix error while upr5

--- HW3/js/script5.js
+++ HW3/js/script5.js
@@ -1,12 +1,14 @@
-let i = 8;
-for (i; i <=56; i++) {
-    if (i % 2 == 0) {
-        console.log(i)
-    }
-}
-
-// while (i <= 56) {
+// let i = 8;
+// for (i; i <=56; i++) {
 //     if (i % 2 == 0) {
-//         console.log(i++);
+//         console.log(i)
 //     }
 // }
+
+let j = 7;
+while (j < 56) {
+    j++;
+    if (j % 2 == 0)
+    console.log(j);
+    
+}
