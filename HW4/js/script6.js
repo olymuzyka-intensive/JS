bbe8714ad46d54c955951823290e3299446bc807
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 15:31:58 +0300

    add upr6

---
+++ HW4/js/script6.js
@@ -0,0 +1,6 @@
+let arr7 = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий'];
+arr7.push('фиолетовый');
+let last = arr7.length;
+console.log(arr7);
+for (let i = 0; i < arr7.length; i++ )
+console.log(arr7[arr7.length-1])
