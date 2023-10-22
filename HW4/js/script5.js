c27ffa2431a46283b64f142c15961f639a708010
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 15:12:19 +0300

    add upr5

---
+++ HW4/js/script5.js
@@ -0,0 +1,7 @@
+let arr6 = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+for (let i = 0; i < arr6.length-2; i++) {
+    document.write(arr6[i]);
+}
+for (let i = arr6.length-2; i < arr6.length; i++) {
+    document.write(arr6[i].bold());
+}
