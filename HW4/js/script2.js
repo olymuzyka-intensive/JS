42ff2fd2241e75544f57aa67476daa378c9e7890
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 14:54:09 +0300

    add upr4

--- HW4/js/script2.js
+++ HW4/js/script2.js
@@ -1,6 +1,6 @@
-let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
-for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] < -3 && arr[i] > -10) {
-        console.log(arr[i]);
-    }
-}
+// let arr2 = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+// for (let i = 0; i <= arr2.length; i++) {
+//     if (arr2[i] < -3 && arr2[i] > -10) {
+//         console.log(arr2[i]);
+//     }
+// }
---
+++ HW4/js/script4.js
@@ -0,0 +1,9 @@
+const arr4 = ['10', '20', '30', '50', '235', '3000'];
+const arr5 = [];
+
+for (let i = 0; i < arr4.length; i++) {
+if (arr4[i].charAt(0) == '1' || arr4[i].charAt(0) == '2' || arr4[i].charAt(0) == '5') {
+    arr5.push(arr4[i]);
+}
+}
+console.log(arr5);
