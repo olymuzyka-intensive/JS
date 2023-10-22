14f217ced4b998cf6c4f9306bb930ebf27aa999e
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-23 00:49:03 +0300

    add upr11

--- HW4/js/script11.js
+++ HW4/js/script11.js
@@ -2,13 +2,28 @@ let res = '';
 let n = 5;
 
 for (let i = 1; i <= n; i++) {
-    for (j = n; j >1; j--) {
-        res +='';
+        for (j = n; j >i; j--) {
+            res +='';
+        }
+        for (j = 1; j <= i; j++) {
+            res += '^'+ '';
+        }
+        res += '\n';
     }
-    for (j = 1; j <= i; j++) {
-        res += '^'+ '';
-    }
-    res += '\n'
-}
-
 console.log(res);
+
+
+
+// for (let i = 1; i <= n; i++) {
+//     for (j = 1; j < n - i; j++) {
+//         res +='';
+//     }
+//     for (let j = 1; j <= i; j++) {
+//         res += '^'+ '';
+//     }
+//     for (let k = 0; k <=2 * i - 1; k++){
+//    console.log('^')
+// }
+
+// console.log('\n');
+// }
