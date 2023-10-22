8a57f339c83cec8c964540a003da438c995c9c84
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 22:12:49 +0300

    fix while problems

--- HW4/js/script8.js
+++ HW4/js/script8.js
@@ -1,4 +1,5 @@
 let arr9 = [12, false, 'text', 4, 2, -5, 0];
+let arr10 = arr9;
 let i = 0;
 for (i = 0; i < arr9.length; i++)
 
@@ -14,7 +15,16 @@ for (i = arr9.length-1; i >=0; i--) {
 console.log(arr9.reverse());
 
 
-// while (arr9.length){
-//     i--;
-//     console.log(arr9[i]);
-// }
+let start = arr9.length - 1;
+let end = 0;
+
+while (start < end) {
+    [arr9[start], arr9[end]] = [arr9[end], arr9[start]];
+    start++;
+    end--;
+}
+console.log(arr9);
+
+
+
+
