068b3fb09a7c55a026262499ceae4dfe622dd5a0
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 22:43:59 +0300

    add upr10

--- HW4/js/script8.js
+++ HW4/js/script8.js
@@ -1,29 +1,29 @@
-let arr9 = [12, false, 'text', 4, 2, -5, 0];
-let arr10 = arr9;
-let i = 0;
-for (i = 0; i < arr9.length; i++)
+// let arr9 = [12, false, 'text', 4, 2, -5, 0];
+// let arr10 = arr9;
+// let i = 0;
+// for (i = 0; i < arr9.length; i++)
 
-{console.log(arr9[i]);}
+// {console.log(arr9[i]);}
 
-console.log('перевернутый');
+// console.log('перевернутый');
 
-for (i = arr9.length-1; i >=0; i--) {
-    console.log(arr9[i]);
-}
+// for (i = arr9.length-1; i >=0; i--) {
+//     console.log(arr9[i]);
+// }
 
    
-console.log(arr9.reverse());
+// console.log(arr9.reverse());
 
 
-let start = arr9.length - 1;
-let end = 0;
+// let start = arr9.length - 1;
+// let end = 0;
 
-while (start < end) {
-    [arr9[start], arr9[end]] = [arr9[end], arr9[start]];
-    start++;
-    end--;
-}
-console.log(arr9);
+// while (start < end) {
+//     [arr9[start], arr9[end]] = [arr9[end], arr9[start]];
+//     start++;
+//     end--;
+// }
+// console.log(arr9);
 
 
 
---
+++ HW4/js/script9.js
@@ -0,0 +1,13 @@
+// let arr11 = [5,9,21,,,9,78,,,,6];
+// let kol = 0;
+
+// console.log(arr11);
+// for (let i = 0; i < arr11.length; i++) {
+//     if (typeof arr11[i] === "undefined"){
+//          kol++;
+//     }
+   
+// }
+
+// console.log(kol);
+
