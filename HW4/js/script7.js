0085f40201060ce927e53a298cddc3273451b952
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 16:07:44 +0300

    add upr7

---
+++ HW4/js/script7.js
@@ -0,0 +1,12 @@
+let arr8 = [];
+alert('введите элементы массива');
+while(true) {
+let n = prompt('ввод данных');
+if (n === '') break;
+arr8.push(+n)
+}
+
+console.log('исходный массив',arr8);
+
+
+console.log('сортировка',arr8.sort());
