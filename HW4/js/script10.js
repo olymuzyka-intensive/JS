d35a49f8a53bc901b4ba095caa20dcb1cb303e0d
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-23 00:18:33 +0300

    fix upr10

---
+++ HW4/js/script10.js
@@ -0,0 +1,32 @@
+let arr12 = [1,8,0,13,76,8,7,0,22,0,2,3,2];
+
+let indexStart = arr12.indexOf(0)+1;
+let indexEnd = arr12.lastIndexOf(0);
+
+let summ = 0;
+
+// console.log(indexStart);
+// console.log(indexEnd);
+
+for (let i = indexStart; i < indexEnd; i++) {
+    summ += arr12[i];
+}
+
+console.log(summ);
+
+
+
+//эксперименты
+// for (let i = 0; i < arr12.length; i++) {
+//     if (arr12[i] == 0) {
+//         indexStart = i;
+//         break;
+//   }
+  
+// for (let i=arr12.length; i >= 0; i--) {
+//     if (arr12[i] == 0) {
+//         indexEnd = i;
+//         break;
+//     }
+  
+// arr12.splice(indexStart, indexEnd).reduce((a,b)=>a+b);
