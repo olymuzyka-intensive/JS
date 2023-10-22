7a9fe0921e3b5da308d8d16f4348d2ca2b405fdb
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-22 17:01:03 +0300

    add upr8 nuzno while proverit

--- HW4/js/script8.js
+++ HW4/js/script8.js
@@ -1,4 +1,20 @@
-let arr = [1, 2, 3, 4, 5];
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+let arr9 = [12, false, 'text', 4, 2, -5, 0];
+let i = 0;
+for (i = 0; i < arr9.length; i++)
+
+{console.log(arr9[i]);}
+
+console.log('перевернутый');
+
+for (i = arr9.length-1; i >=0; i--) {
+    console.log(arr9[i]);
 }
+
+   
+console.log(arr9.reverse());
+
+
+// while (arr9.length){
+//     i--;
+//     console.log(arr9[i]);
+// }
