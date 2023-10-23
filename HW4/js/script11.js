82c86180a33097c50db9906978c14544f28dec8b
olymuzyka-intensive <olymuzyka@gmail.com>
2023-10-23 10:23:13 +0300

    fix upr11 2 variant

--- HW4/js/script11.js
+++ HW4/js/script11.js
@@ -1,17 +1,33 @@
+
+let h = prompt('введите высоту 1 треугольника','');
+let ln = "&nbsp"
+for (let i = 0; i < h; i++) {
+  for (let k = h; k > i - 1; k--) {
+    document.write(ln, ln)
+  }
+  for (let j = 0; j < i + 1; j++) {
+    document.write("^", ln, ln)
+  }
+  document.write("<br>")
+}
+
+
+
+//эксперименты
 let res = '';
-let n = 5;
+let n = prompt('введите высоту 2 треугольника (результат в консоли)','');
 
-for (let i = 1; i <= n; i++) {
-        for (j = n; j >i; j--) {
-            res +='';
-        }
-        for (j = 1; j <= i; j++) {
-            res += '^'+ '';
-        }
-        res += '\n';
+
+for (let i = 0; i < n; i++) {
+    for (let j = n; j > i - 1; j--) {
+        res +='';
     }
+    for (let k = 0; k < i + 1; k++) {
+        res += '^'+ '';
+    }
+    res += '\n';
+}
 console.log(res);
-
 
 
 // for (let i = 1; i <= n; i++) {
@@ -27,3 +43,18 @@ console.log(res);
 
 // console.log('\n');
 // }
+
+
+
+// let j = 0;
+
+// for (let i = 0; i < n; i++) {
+//         for (let j = n; j > i-1; j--) {
+//             document.write(res, res);
+//         }
+//         for (let k = 0; k <= i+1; k++) {
+//             document.write('^', res, res);
+//         }
+//         document.write('\n');
+//     }
+//     document.write(res);
