ee7762e73f8f5161d337b7f1a8269592e04a88a8
olymuzyka-intensive <olymuzyka@gmail.com>
2023-11-01 01:27:04 +0300

    add upr3

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ let newDate = arr[2] + '/' + arr[1] + '/' + arr[0];
 
 console.log('исходная дата', date);
 console.log('дата в другом формате', newDate);
+
+// 3. Дана строка «Я учу javascript!». Вырежете из нее слово «учу» и слово «javascript» тремя разными способами (через substr, substring, slice).
+
+
+
+let str1 = 'Я учу javascript!';
+let result1 = str1.substring(0,1)+str1.substring(16,17);
+
+let str2 = 'Я учу javascript!';
+let result2 = str2.substr(0,1) + str2.substr(16,1);
+
+let str3 = 'Я учу javascript!';
+let result3 = str3.slice(0,1)+str3.slice(16,17);
+console.log(result1);
+console.log(result2);
+console.log(result3);
