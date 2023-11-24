d30de6b3711dd75577fcab564dcd8768f0e28860
olymuzyka-intensive <olymuzyka@gmail.com>
2023-11-24 23:11:44 +0300

    add  checkbox and btnedit

--- HW9/scrript.js
+++ HW9/scrript.js
@@ -5,20 +5,27 @@ let add = document.querySelector('.btn-add');
 add.addEventListener('click', addNewLi);
 
 function addNewLi () {
-    
-
     let newLi = document.createElement('li'); //создание элемента
     newLi.className = 'addadLi';
     value = document.querySelector('.write_shopping').value;
-    // let textLi = newLi.value;
     newLi.textContent = value;
     shopList.appendChild(newLi); //добавление элемента
 
     let input = document.querySelector('.write_shopping'); 
     input.value = ''; //очищение поля после ввода
 
+    let checkBoxElem =document.createElement('checkBox');
+    // checkBoxElem.setAttribute('type', 'checkbox')
+    // checkBoxElem.before(newLi.value);
+    checkBoxElem.innerHTML = '&#10008'
+    newLi.before(checkBoxElem);
 
+    let editLi = document.createElement('button');
+    editLi.className = 'btn-edit'
+    editLi.innerHTML = '&#10000';
+    newLi.append(editLi);
 }
+
 
 
 
