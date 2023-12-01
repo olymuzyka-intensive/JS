fc8fcddc18e9cdcb1867610d57d64b8c103a2278
olymuzyka-intensive <olymuzyka@gmail.com>
2023-12-01 20:53:52 +0300

    fix problems

--- HW10/script.js
+++ HW10/script.js
@@ -1,16 +1,16 @@
-const Book = function(title = '', category = '', author = '', storage = '')
-{
+const Book = function(title = '', category = '', author = '', rating = ''){
     this.title = title;
     this.category = category;
     this.author = author;
-    this.storage = storage;
-}
+    this.rating = rating;
+
 
 this.show = function(){
-    document.write ('<li><strong>${this.title}</strong>: <i>${this.category}</i><strong><i>${this.author}</i></strong><u>${this.storage}</u> </li>');
+    console.log(`${this.title} ${this.author} (${this.category}) ${this.rating}`);
+    // document.write ('<li><strong>${this.title}</strong>: <i>${this.category}</i><strong><i>${this.author}</i></strong><u>${this.rating}</u> </li>');
+    }
 }
-
-const MyBibl = function(){
+const MyBooks = function(){
     this.data = [];
 
     this.add = function(Book = {}){
@@ -22,24 +22,23 @@ const MyBibl = function(){
 
         this.data.forEach(function(item){
             item.show();
-        })
+        });
 
         // document.write('</ul>');
     }
 };
 
-let myBibl = new MyBibl();
+let myBooks = new MyBooks();
 
-myBibl.add(new MyBibl('Огненная вспышка','детектив','Ричард Старк','в читальном зале'));
-myBibl.add(new MyBibl('Воздушный витязь','проза','Игорь Соркин','on-line по запросу'));
-myBibl.add(new MyBibl('Дракон Фенстонов','приключения','Анри Верн','в читальном зале'));
-myBibl.add(new MyBibl('Заколдованное счастье','поэзия','Лев Маляков','в читальном зале'));
-myBibl.add(new MyBibl('Игры теней','фантастика','Ерофей Трофимов','on-line по запросу'));
-myBibl.add(new MyBibl('Пропавшие','детектив','Джейн Кейси','в читальном зале'));
-myBibl.add(new MyBibl('Книга чудес','проза','Натаниель Готорн','в читальном зале'));
-myBibl.add(new MyBibl('Разум океана','приключения','Станислав Гагарин','в читальном зале'));
-myBibl.add(new MyBibl('Мгновения Вечности','поэзия','Владимир Кевхишвили','on-line по запросу'));
-myBibl.add(new MyBibl('Прибытие на Марс','фантастика','Иван Беров','в читальном зале'));
+myBooks.add(new Book('Огненная вспышка','детектив','Ричард Старк', 5));
+myBooks.add(new Book('Воздушный витязь','проза','Игорь Соркин', 4));
+myBooks.add(new Book('Дракон Фенстонов','приключения','Анри Верн', 3));
+myBooks.add(new Book('Заколдованное счастье','поэзия','Лев Маляков', 5));
+myBooks.add(new Book('Игры теней','фантастика','Ерофей Трофимов', 5));
+myBooks.add(new Book('Пропавшие','детектив','Джейн Кейси', 4));
+myBooks.add(new Book('Книга чудес','проза','Натаниель Готорн', 4));
+myBooks.add(new Book('Разум океана','приключения','Станислав Гагарин', 5));
+myBooks.add(new Book('Мгновения Вечности','поэзия','Владимир Кевхишвили', 3));
+myBooks.add(new Book('Прибытие на Марс','фантастика','Иван Беров', 2));
 
-
-myBibl.show();
+myBooks.show();
